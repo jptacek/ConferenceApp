3dd6f20be5777beb09952a3323ad4594193c5e5a
jptacek@live.com <jptacek@live.com>
2012-03-27 05:40:32 -0500

    Initial project setup

---
+++ ConferenceApp/ConferenceApp/www/stopGap.js
@@ -0,0 +1,12 @@
+﻿function StopGap() {
+    function deviceready() {
+        // fire off deviceready
+        var e = document.createEvent('Events');
+        e.initEvent('deviceready');
+        document.dispatchEvent(e);
+    }
+
+    window.PhoneGap = {};
+    window.device = { cordova: "In browser" };
+    deviceready();
+}
